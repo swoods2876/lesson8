3625e7296fd14e555cef27d6f4627095091300c8
swoods2876 <stephen.wooods@gmail.com>
2015-06-11 20:29:21 -0400

    First commit

---
+++ javascript/scripts.js
@@ -0,0 +1,21 @@
+
+
+	function validateForm() {
+    var x = document.forms["myForm"]["name"].value;
+    
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    
+     }   
+    }
+
+
+function myFunction() {
+    var Blah = document.getElementById("Zipcode");
+    if (Blah.checkValidity() == false) {
+        document.getElementById("Zip").innerHTML = Blah.validationMessage;
+    } else {
+        document.getElementById("Zip").innerHTML = "Input OK";
+    } 
+} 
