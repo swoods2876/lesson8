f9c8012555cc690ade79792ed9322666391b4a78
Abigail Graszl <acgraszl@gmail.com>
2015-06-15 23:57:37 -0400

    added a bit of syling and javascript to donate page

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,20 +1,39 @@
+$(document).ready(function(){
 
 
-	function validateForm() {
-    var x = document.forms["myForm"]["name"].value;
-    if (x == null || x == "") {
-        alert("Name must be filled out");
-        return false;
-    
-     }   
-    }
+   
+    $("#Name").click(function validateForm(){
+
+        var x = document.forms["myForm"]["name"].value;
+       
+        if (x == null || x == "") {
+            alert("Name must be filled out");
+            return false; 
+        } 
+    });
 
 
-function myFunction() {
+
+    $("#Zip").click(function myFunction(){
+
     var Blah = document.getElementById("Zipcode");
     if (Blah.checkValidity() == false) {
         document.getElementById("Zip").innerHTML = Blah.validationMessage;
     } else {
         document.getElementById("Zip").innerHTML = "Thank You SOUPerteer";
-    } 
-} 
+    }
+
+    })
+
+
+    $("#GiveMoney").click(function(){
+        $("html, body").css({
+                'overflow': 'hidden',
+                'height': '100$'
+             });
+       
+            $("#lightbox").fadeIn();
+     });
+            
+
+});
